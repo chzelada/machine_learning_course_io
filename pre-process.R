ef34be0d3c0b56959676b054f462030ca32a8f85
czelada <czelada@healthcare.com>
2018-05-08 08:33:03 -0600

    added preprocess file

---
+++ pre-process.R
@@ -0,0 +1,50 @@
+## Dummy Variables
+
+library(earth)
+data(etitanic)
+dummies <- dummyVars(survived ~ ., data = etitanic)
+head(predict(dummies, newdata = etitanic))
+new_data <- predict(dummies, newdata = etitanic)
+
+
+## zero or near-zero variance
+
+data(mdrr)
+table(mdrrDescr$nR11)
+nzv <- nearZeroVar(mdrrDescr, saveMetrics= TRUE)
+sum(nzv$zeroVar)
+sum(nzv$nzv)
+nzv$freqRatio
+nzv[!nzv$nzv,][1:10,]
+
+nzv <- nearZeroVar(mdrrDescr)
+clean_mdrrDescr <- mdrrDescr[,-nzv]
+
+
+
+## Correlated Predictors
+
+descrCor <-  cor(clean_mdrrDescr)
+highCorr <- sum(abs(descrCor[upper.tri(descrCor)]) > .999)
+summary(descrCor[upper.tri(descrCor)])
+
+descrCor <-  cor(clean_mdrrDescr)
+highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
+clean_mdrrDescr <- clean_mdrrDescr[,-highlyCorDescr]
+
+## Center and Scale
+
+set.seed(96)
+inTrain <- sample(seq(along = mdrrClass), length(mdrrClass)/2)
+
+training <- filteredDescr[inTrain,]
+test <- filteredDescr[-inTrain,]
+trainMDRR <- mdrrClass[inTrain]
+testMDRR <- mdrrClass[-inTrain]
+
+preProcValues <- preProcess(training, method = c("center", "scale"))
+
+trainTransformed <- predict(preProcValues, training)
+testTransformed <- predict(preProcValues, test)
+
+
